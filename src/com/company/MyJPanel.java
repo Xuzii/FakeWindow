8cc0ee283113a4234dabeb2b7fd285ab9321eaa2
Kenneth Ruslim <kenneth.anthony@ruslim.net>
2018-09-15 18:12:39 -0700

    Finished

--- src/com/company/MyJPanel.java
+++ src/com/company/MyJPanel.java
@@ -2,16 +2,48 @@ package com.company;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class MyJPanel extends JPanel {
+public class MyJPanel extends JPanel implements MouseListener{
+
+    public static WindowManager windowManager;
+
     public MyJPanel() {
-        WindowManager windowManager = new WindowManager();
-        //windowManager.display(g);
+        windowManager = new WindowManager();
+        addMouseListener(this);
     }
-
-    protected void paintCompenent(Graphics g){
+    @Override
+    protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+        windowManager.display(g);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
 
     }
 
+    @Override
+    public void mousePressed(MouseEvent e) {
+        windowManager.bringToFront(e.getX(),e.getY());
+        System.out.println(e.getX() + " | " + e.getY());
+        paintComponent(this.getGraphics());
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
 }
--- src/com/company/WindowManager.java
+++ src/com/company/WindowManager.java
@@ -4,7 +4,7 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class WindowManager {
-    private ArrayList<Window> storeWindows;
+    private ArrayList<Window> storeWindows = new ArrayList<>();
     public final int WINDOW_COUNT = 11;
 
     public WindowManager() {
@@ -16,6 +16,17 @@ public class WindowManager {
         for(Window window : storeWindows){
             g.setColor(window.getColor());
             g.fillRect(window.getxPos(),window.getyPos(),window.getxSize(),window.getySize());
+        }
+    }
+    public void bringToFront(int xClick, int yClick){
+        for(int i = 0; i < WINDOW_COUNT; i++){
+            if((xClick > storeWindows.get(i).getxPos() && xClick < storeWindows.get(i).getxPos() + storeWindows.get(i).getxSize()) &&
+                    (yClick > storeWindows.get(i).getyPos() && yClick < storeWindows.get(i).getyPos() + storeWindows.get(i).getySize())){
+                Window inefficientWindow = storeWindows.get(i);
+                storeWindows.remove(storeWindows.get(i));
+                storeWindows.add(0,inefficientWindow);
+                System.out.println("MOVE");
+            }
         }
 
     }
